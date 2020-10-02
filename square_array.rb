0ab76e3805bf429064906a1c00e471c3f6d0245a
RTBlanco <ronnytoribio1@hotmail.com>
2020-10-02 15:02:52 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |number|
+    new_array << number ** 2
+  end
+  return new_array
 end
+
+# def square_array(array)
+#   # """ this is another version just for practice """
+#   new_array = []
+#   array.each{|number| new_array << number ** 2}
+#   return new_array
+# end
